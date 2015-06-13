8b05ed7fc86104af21b7b08c1e1cfd171928c235
Terell Brown <terellbrown.93@gmail.com>
2015-06-13 01:19:13 -0600

    removed/ all strings

--- app/src/main/java/jdm/walkietalkie/test.java
+++ app/src/main/java/jdm/walkietalkie/test.java
@@ -4,10 +4,5 @@ package jdm.walkietalkie;
  * Created by Jon on 2015-06-13.
  */
 public class test {
-    public String g1;
-    public String rerre;
-    public String x;
-    public String t;
-    public String b;
-    String test;
+
 }
