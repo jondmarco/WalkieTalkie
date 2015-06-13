9668d535efd70650634a5d66c69095a565ce6ef3
jontheking33 <jontheking33@hotmail.com>
2015-06-13 14:24:14 -0600

    push

--- app/src/main/java/jdm/walkietalkie/MainActivity.java
+++ app/src/main/java/jdm/walkietalkie/MainActivity.java
@@ -8,6 +8,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.media.AudioFormat;
+import android.media.AudioRecord;
+import android.media.MediaRecorder;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -20,6 +25,8 @@ import android.widget.ListView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
 
@@ -33,6 +40,29 @@ public class MainActivity extends ActionBarActivity implements ListView.OnClickL
     private final UUID APP_UUID = UUID.fromString("fa87c0d0-afac-11de-8a39-0800200c9a66");
     ConnectPhoneThread connectPhoneThread;
     AcceptThread acceptThread;
+    ConnectedThread connectedThread;
+    private final int SUCCESS_CONNECT = 0;
+    private final int SEND_MIC_AUDIO = 1;
+    private AudioRecord recorder = null;
+
+    private Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case SUCCESS_CONNECT:
+                    connectedThread = new ConnectedThread(
+                            (BluetoothSocket) msg.obj);
+                    connectedThread.start();
+                    break;
+                case SEND_MIC_AUDIO:
+
+
+                    connectedThread.write((byte[]) msg.obj);
+                    break;
+            }
+        }
+    };
 
     protected Context activityContext;
 
@@ -205,6 +235,7 @@ public class MainActivity extends ActionBarActivity implements ListView.OnClickL
             // Do work to manage the connection (in a separate thread)
             //ConnectedThread connectedThread = new ConnectedThread(mmSocket);
             //connectedThread.start();
+            mHandler.obtainMessage(SUCCESS_CONNECT, mmSocket).sendToTarget();
         }
 
         /**
@@ -250,7 +281,8 @@ public class MainActivity extends ActionBarActivity implements ListView.OnClickL
                     // Do work to manage the connection (in a separate thread)
                     //ConnectedThread connectedThread = new ConnectedThread(socket);
                     //connectedThread.start();
-                    //Toast.makeText(activityContext,"Connected",Toast.LENGTH_LONG).show();
+                    connectedThread = new ConnectedThread(socket);
+                    connectedThread.start();
                     try {
                         mmServerSocket.close();
                     } catch (IOException e) {
@@ -260,5 +292,109 @@ public class MainActivity extends ActionBarActivity implements ListView.OnClickL
                 }
             }
         }
+    }
+
+    private class ConnectedThread extends Thread {
+
+        private final BluetoothSocket mmSocket;
+        private final InputStream mmInStream;
+        private final OutputStream mmOutStream;
+        private static final int RECORDER_SAMPLERATE = 8000;
+        private static final int RECORDER_CHANNELS = AudioFormat.CHANNEL_IN_MONO;
+        private static final int RECORDER_AUDIO_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
+        private boolean isRecording = false;
+        private int[] mSampleRates = new int[] { 8000, 11025, 22050, 44100 };
+        int bufferSize;
+
+        public ConnectedThread(BluetoothSocket socket) {
+            mmSocket = socket;
+            InputStream tmpIn = null;
+            OutputStream tmpOut = null;
+
+            // Get the input and output streams, using temp objects because
+            // member streams are final
+            try {
+                tmpIn = socket.getInputStream();
+                tmpOut = socket.getOutputStream();
+            } catch (IOException e) { }
+
+            mmInStream = tmpIn;
+            mmOutStream = tmpOut;
+        }
+
+        //convert short to byte
+        private byte[] short2byte(short[] sData) {
+            int shortArrsize = sData.length;
+            byte[] bytes = new byte[shortArrsize * 2];
+            for (int i = 0; i < shortArrsize; i++) {
+                bytes[i * 2] = (byte) (sData[i] & 0x00FF);
+                bytes[(i * 2) + 1] = (byte) (sData[i] >> 8);
+                sData[i] = 0;
+            }
+            return bytes;
+
+        }
+
+        public AudioRecord findAudioRecord() {
+            for (int rate : mSampleRates) {
+                for (short audioFormat : new short[] { AudioFormat.ENCODING_PCM_8BIT, AudioFormat.ENCODING_PCM_16BIT }) {
+                    for (short channelConfig : new short[] { AudioFormat.CHANNEL_IN_MONO, AudioFormat.CHANNEL_IN_STEREO }) {
+                        try {
+
+                            bufferSize = AudioRecord.getMinBufferSize(rate, channelConfig, audioFormat);
+
+                            if (bufferSize != AudioRecord.ERROR_BAD_VALUE) {
+                                // check if we can instantiate and have a success
+                                AudioRecord recorder = new AudioRecord(MediaRecorder.AudioSource.DEFAULT, rate, channelConfig, audioFormat, bufferSize);
+
+                                if (recorder.getState() == AudioRecord.STATE_INITIALIZED)
+                                    return recorder;
+                            }
+                        } catch (Exception e) {
+
+                        }
+                    }
+                }
+            }
+            return null;
+        }
+
+
+
+        public void run() {
+              // buffer store for the stream
+            int bytes; // bytes returned from read()
+            int BytesPerElement = 2; // 2 bytes in 16bit format
+
+
+            recorder = findAudioRecord();
+            //recorder.release();
+
+            short sData[] = new short[bufferSize];
+            recorder.startRecording();
+            // Keep listening to the InputStream until an exception occurs
+            while (true) {
+                // Read from the InputStream
+                bytes = recorder.read(sData, 0, bufferSize);
+                // Send the obtained bytes to the UI activity
+                mHandler.obtainMessage(SEND_MIC_AUDIO, bytes, -1, bufferSize).sendToTarget();
+                //write(short2byte(sData));
+            }
+        }
+
+        /* Call this from the main activity to send data to the remote device */
+        public void write(byte[] bytes) {
+            try {
+                mmOutStream.write(bytes);
+            } catch (IOException e) { }
+        }
+
+        /* Call this from the main activity to shutdown the connection */
+        public void cancel() {
+            try {
+                mmSocket.close();
+            } catch (IOException e) { }
+        }
+
     }
 }
