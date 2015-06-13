fcfffbabb6e468c4bb7ee3f4f59020cb36acb715
jontheking33 <jontheking33@hotmail.com>
2015-06-13 01:01:29 -0600

    test

--- app/src/main/java/jdm/walkietalkie/test.java
+++ app/src/main/java/jdm/walkietalkie/test.java
@@ -4,7 +4,7 @@ package jdm.walkietalkie;
  * Created by Jon on 2015-06-13.
  */
 public class test {
-    public String g;
+    public String g1;
     public String r;
     public String x;
     public String t;
