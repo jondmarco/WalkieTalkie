91aeab0477754841367e27d9eb8876c2b4df4b8f
jontheking33 <jontheking33@hotmail.com>
2015-06-21 17:26:03 -0600

    test

---
+++ app/src/main/java/jdm/walkietalkie/Util/HandlerCases.java
@@ -0,0 +1,9 @@
+package jdm.walkietalkie.Util;
+
+/**
+ * Created by Jon on 2015-06-15.
+ */
+public class HandlerCases {
+    public static final int SUCCESS_CONNECT = 0;
+    public static final int RECEIVE_AUDIO = 1;
+}
